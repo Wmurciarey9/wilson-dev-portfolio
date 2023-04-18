73209cb8c9bb572a776b9658f10be6e0f1548c60
Wilson <wmurciarey9@gmail.com>
2023-04-17 20:36:27 -0500

    Changed cv file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import React, { useContext, useState } from "react";
 import { ThemeContext } from "../../context";
-import resume from "./resume.pdf";
+import resume from "./CVWilsonDev.pdf";
 
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css"; // optional
@@ -27,13 +27,20 @@ export const Navbar = () => {
   return (
     <div
       className={isScrolled ? "navbar scrolled" : "navbar"}
-      style={{ backgroundColor: darkMode ? "#303030" : "", color: darkMode && "whitesmoke" }}
+      style={{
+        backgroundColor: darkMode ? "#303030" : "",
+        color: darkMode && "whitesmoke",
+      }}
     >
       <div className="container">
         <div className="left">
           <Link to={`/`}>
             <Tippy content="Home">
-              <img src={Logo} alt="" style={{ content: darkMode && 'url("' + WhiteLogo + '")' }} />
+              <img
+                src={Logo}
+                alt=""
+                style={{ content: darkMode && 'url("' + WhiteLogo + '")' }}
+              />
             </Tippy>
           </Link>
         </div>
@@ -41,21 +48,30 @@ export const Navbar = () => {
           <div className="iconItem">
             <a href="https://github.com/Wmurciarey9" target="_blank">
               <Tippy content="GitHub">
-                <GitHubIcon className="icon" style={{ color: darkMode && "#fff" }} />
+                <GitHubIcon
+                  className="icon"
+                  style={{ color: darkMode && "#fff" }}
+                />
               </Tippy>
             </a>
           </div>
           <div className="iconItem">
             <a href="https://www.linkedin.com/in/wmurciarey" target="_blank">
               <Tippy content="LinkedIn">
-                <LinkedInIcon className="icon" style={{ color: darkMode && "#fff" }} />
+                <LinkedInIcon
+                  className="icon"
+                  style={{ color: darkMode && "#fff" }}
+                />
               </Tippy>
             </a>
           </div>
           <div className="iconItem">
             <a href={resume} download="resume.pdf">
               <Tippy content="Resume">
-                <InsertDriveFileIcon className="icon" style={{ color: darkMode && "#fff" }} />
+                <InsertDriveFileIcon
+                  className="icon"
+                  style={{ color: darkMode && "#fff" }}
+                />
               </Tippy>
             </a>
           </div>
